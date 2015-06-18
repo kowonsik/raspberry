f93ab7d69e1fed4389a9bd23704a5cdf6cfd4c1c
kowonsik <kows17710@gmail.com>
2015-06-19 08:39:00 +0900

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -8,8 +8,17 @@ import time
 import traceback
 import requests
 
+import RPi.GPIO as GPIO
+
 
 SERVER_ADDR = '211.184.76.80'
+
+GPIO.setwarnings(False)
+GPIO.cleanup()
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(19,GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
 
 def query_last_data_point(plug_id):
 	url = 'http://%s/api/raw_plug_last/?plug_id=%d' % (SERVER_ADDR, plug_id)
@@ -35,12 +44,18 @@ while True:
 		t, v = ret
 		if t > time.time() - 30:
 			dt = time.time() - t 
-			print 'plug %d operate correctly. (%.1f seconds ago, %f watt)' % (plug_id, dt, v)
+			#print 'plug %d operate correctly. (%.1f seconds ago, %f watt)' % (plug_id, dt, v)
+			GPIO.out(19, True)
+			GPIO.out(26, False)
 
 		else:
-			print 'plug %d may be not connected to server. (%.1f seconds ago, %f watt)' % (plug_id, dt, v)
+			#print 'plug %d may be not connected to server. (%.1f seconds ago, %f watt)' % (plug_id, dt, v)
+			GPIO.out(19, True)
+			GPIO.out(26, False)
 
 	else:
-		print 'plug %d does not exists on server!' % plug_id
+		#print 'plug %d does not exists on server!' % plug_id
+		GPIO.out(19, False)
+		GPIO.out(26, True)
 
 	time.sleep(3.0)
