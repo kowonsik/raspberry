4975f361af10f95500a87d171d6c64a913c19e6e
kowonsik <kows17710@gmail.com>
2015-06-22 19:01:18 +0900

    refactoring

--- secret.py
+++ secret.py
@@ -3,17 +3,12 @@ def secret(plug_id):
 	plug_secret = (plug_id * 2654435761) % 8192
 	return plug_secret
 
-
-print map(secret, [100101, 100103, 100104])
-print map(secret, [100201, 100203, 100204])
-print map(secret, [100301, 100303, 100304])
-print map(secret, [100401, 100403, 100404])
-print map(secret, [100501, 100503, 100504])
-
-print map(secret, [100601, 100603, 100604])
-print map(secret, [100701, 100703, 100704])
-print map(secret, [100801, 100803, 100804])
-print map(secret, [100901, 100903, 100904])
-print map(secret, [101001, 101003, 101004])
-
+for gw in range(10) :
+    home = gw+1001
+    home1 = home*100 + 1
+    home3 = home*100 + 3
+    home4 = home*100 + 4
+    print "HOME=", home, 
+    print map(secret, [home1, home3, home4])
+    print " "
 
