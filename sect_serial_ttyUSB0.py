b37d25a53713ac44ba30af306882164d5d296de8
kowonsik <kows17710@gmail.com>
2015-07-13 22:47:31 +0900

    Update sect_serial_ttyUSB0.py

--- sect_serial_ttyUSB0.py
+++ sect_serial_ttyUSB0.py
@@ -70,17 +70,17 @@ def sese(s):
         humidity = bigEndian( s[52:56] )
         light = bigEndian( s[56:60] )
         #v1 = -46.85 + 0.01 * temperature
-        #tmp = -6 + 125 * humidity / 4095
-        #v2 = tmp
+        tmp = -6 + 125 * humidity / 4095
+        v2 = tmp
         tmp = (light * 1.017)
         v3 = tmp
         
         v1 = -46.85 + 175.72 * float(temperature) / pow(2,16)
-    	v2 = -6 + 125 * float(humidity) / pow(2,16)
+    	#v2 = -6 + 125 * float(humidity) / pow(2,16)
         
 
         t = int(time.time())
-        print "gyu_RC1_thl.temperature %d %.6f nodeid=%d" % ( t, v1, bigEndian( nodeID ) )
+        print "gyu_RC1_thl.temperature %d %.6f nodeid=%d" % ( t, v1*(-1), bigEndian( nodeID ) )
         print "gyu_RC1_thl.humidity %d %.6f nodeid=%d" % ( t, v2, bigEndian( nodeID ) )
         print "gyu_RC1_thl.light %d %f nodeid=%d" % ( t, v3, bigEndian( nodeID ) )
 
